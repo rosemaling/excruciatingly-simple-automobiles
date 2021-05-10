b7d2eaeb1df0f55f8ae18f70aca321e2d52ac3de
Rose Brame <gillianbrame@gmail.com>
2021-05-10 12:08:40 -0500

    init

---
+++ src/main/java/com/galvanize/autos/AutosApiApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.autos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutosApiApplication.class, args);
+	}
+
+}
